7718ef0c1c7d91c034f90ba48a565359db833686
rompy1 <rompyone@gmail.com>
2018-10-11 11:37:00 -0700

    Update Battle2.py
    Fixed indentation errors

--- Battle2.py
+++ Battle2.py
@@ -1,18 +1,18 @@
 import random
 
 def game_over():
-	letter = input("Would you like to play again?: y or n\n")
-	if letter == "y":
-		return 1
-	if letter == "n":
-		exit()
+  letter = input("Would you like to play again?: y or n\n")
+  if letter == "y":
+    return 1
+  if letter == "n":
+    exit()
     
     
 #variables: player's HP, player's attack, player's defense, monster's HP, monster's attack, monster's defense, monster's name, if you can run or not (1 for yes, 0 for no)  
 def fight(Player, Monster):
   Php = Player["hp"]
   Patk = Player["atk"]
-  Pdef1 = Player["def"] 
+  Pdef = Player["def"] 
   Mhp = Monster["hp"]
   Matk = Monster["atk"]
   Mdef = Monster["def"] 
@@ -22,68 +22,67 @@ def fight(Player, Monster):
   print("Your HP = %d" % Php)
   print("Enemy's HP = %d" % Mhp)
   while Php > 0 and Mhp > 0:
-		#player attack
-		while True:
-			option = input("Options: \n1.Attack \n2.Run\n")
-			print(option)
-			if option == "1":
-				print("You attack")
-				print("Damage: %d" % (Patk - Mdef1))
-				if (Patk - Mdef1) > -1:
-					Mhp -= (Patk - Mdef1)
-				if Mhp < 0:
-					Mhp = 0
-				print("Enemy's HP: %d" % Mhp)
-				break
-			if option == "2":
-				print("Attempting to run")
-				if canrun == 1:
-					run = random.randint(0,3)
-					if run == 3:
-						print("sucess!")
-						return 2
-					else:
-						print("failure!")
-						break
+    #player attack
+    while True:
+      option = input("Options: \n1.Attack \n2.Run\n")
+      print(option)
+      if option == "1":
+        print("You attack")
+        print("Damage: %d" % (Patk - Mdef))
+        if (Patk - Mdef) > -1:
+          Mhp -= (Patk - Mdef)
+        if Mhp < 0:
+          Mhp = 0
+        print("Enemy's HP: %d" % Mhp)
+        break
+      if option == "2":
+        print("Attempting to run")
+        if canrun == 1:
+          run = random.randint(0,3)
+          if run == 3:
+            print("sucess!")
+            return 2
+          else:
+            print("failure!")
+            break
             
-		#monster attack:
-		if Mhp > 0:
-				print("The %s attacks" % enemy)
-				if (Matk - Pdef1) > -1:
-					Php -= (Matk - Pdef1)
-				if Php < 0:
-					Php = 0
-				print("HP: %d" % Php)
-	if Php <= 0:
-		print ("You died! Game over.")
-		return game_over()
-	if Mhp <= 0:
-		print("You win the battle!")
-		return 2
+    #monster attack:
+    if Mhp > 0:
+        print("The %s attacks" % enemy)
+        if (Matk - Pdef) > -1:
+          Php -= (Matk - Pdef)
+        if Php < 0:
+          Php = 0
+        print("HP: %d" % Php)
+  if Php <= 0:
+    print ("You died! Game over.")
+    return game_over()
+  if Mhp <= 0:
+    print("You win the battle!")
+    return 2
     
 def CharacterCreate():
   Pname = input("Welcome to character creation! To get started, what would you like to be called?")
 
     
-#example fight: fight(Php, Patk, Pdef1, 20, 5, 2, Giant Spider, 1)
+#example fight: fight(Php, Patk, Pdef, 20, 5, 2, Giant Spider, 1)
 
 #Delete code below to create own fights, this is just an example
 
 Giant_Spider = {
-  "Mhp" : 20
-  "Matk" : 5
-  "Mdef" :  2
-  "enemy" : "Giant Spider"
-  "canrun" : 1 }
+  "hp" : 20,
+  "atk" : 5,
+  "def" :  2,
+  "name" : "Giant Spider",
+  "canrun" : 1, }
 
 Player = {
-  "Php" : 30
-  "Patk" : 10
-  "Pdef1" : 4
+  "hp" : 30,
+  "atk" : 10,
+  "def" : 4,
 }
 
 fight(Player, Giant_Spider)
-
 
 
 
