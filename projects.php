547181455c91d26d702f6f10730f2b42d20243ea
AmaanSingh <42991924+AmaanSingh@users.noreply.github.com>
2018-11-21 15:50:53 -0800

    Update projects.php

--- projects.php
+++ projects.php
@@ -31,11 +31,11 @@
 </div>
 	</nav>
 	<div class = "block">
-		<div><button class = "block"><a href ="text.html">Adventure Text Based game: This is a beginner project coded using python.</a></button></div>
-		<div><button class = "block"><a href = "rock.html">Fun Randomly generated rock, paper, and scissors game. This game is coded using python.</a></button></div>
-		<div><button class= "block"><a href="numberguesser.html"> Create an easy and fun number guessing. You can copy ours word for word, or create you own version.</a></button></div>
-		<div><button class= "block"><a href="password.html"> Need a password generator, here it is. This code lets you generate a random password. </a></button></div>
-		<div><button class= "block"><a href="math.html">Check the mean, medium, and mode of a group of numbers or points. This a great tool for math.</a></button></div>
+		<div><button class = "block"><a href ="text.php">Adventure Text Based game: This is a beginner project coded using python.</a></button></div>
+		<div><button class = "block"><a href = "rock.php">Fun Randomly generated rock, paper, and scissors game. This game is coded using python.</a></button></div>
+		<div><button class= "block"><a href="numberguesser.php"> Create an easy and fun number guessing. You can copy ours word for word, or create you own version.</a></button></div>
+		<div><button class= "block"><a href="password.php"> Need a password generator, here it is. This code lets you generate a random password. </a></button></div>
+		<div><button class= "block"><a href="math.php">Check the mean, medium, and mode of a group of numbers or points. This a great tool for math.</a></button></div>
 
 	</div>
 <div class = "Sign">
@@ -43,8 +43,8 @@
 </div>
 <div class = "footer">
 	<div><li><a href = "https://github.com/AmaanSingh"><img src="glyphicons-social-22-github.png"></li></div>
-	<div><li><a href = "contact.html"><img src="glyphicons-social-14-e-mail-envelope.png"></a></li></div>
-	<div><li><a href = "disclaimer.html">Disclaimer</a></li></div>
+	<div><li><a href = "contact.php"><img src="glyphicons-social-14-e-mail-envelope.png"></a></li></div>
+	<div><li><a href = "disclaimer.php">Disclaimer</a></li></div>
 	<div class = "computer"><img src="logo.png"></div>
 	<div class = "jshs"><img src = "jshslogo.png"></div>
 
