500a7e9bd3ccc966a3b10bbea2d4074ce4968c9a
AmaanSingh <42991924+AmaanSingh@users.noreply.github.com>
2018-11-21 15:54:48 -0800

    Update contact_me.php

--- contact_me.php
+++ contact_me.php
@@ -1,16 +1,24 @@
 <?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
 
-if (isset($_POST['submit'])) {
 $name = $_POST['name'];
-$subject = $_POST['subject'];
-$mailFrom = $_POST['mail'];
+$email_address = $_POST['email'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$mailto = "amaansingh160@gmail.com"; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$headers = "From: ".$mailFrom;
-$txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
-	
-mail($mailTo, $subject, $txt, $headers);
-header("Location: contact.php?mailsend");
-}
+$to = 'amaansingh160@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+return true;
+?>
