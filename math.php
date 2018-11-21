7fdf7aa36759c90bc102d95f47fe3c302e20dbc1
AmaanSingh <42991924+AmaanSingh@users.noreply.github.com>
2018-11-21 15:45:25 -0800

    Update math.php

--- math.php
+++ math.php
@@ -58,10 +58,10 @@
 	<img src = "logo.png">
 </div>
 		<ul>
-			<li><a href ="index.html">HOME</a></li>
-			<li><a href ="projects.html">PROJECTS</a></li>
-			<li><a href ="contact.html">CONTACT</a></li>
-			<button class = "navy"><a href ="index.html"> Contact Us</a></button>
+			<li><a href ="index.php">HOME</a></li>
+			<li><a href ="projects.php">PROJECTS</a></li>
+			<li><a href ="contact.php">CONTACT</a></li>
+			<button class = "navy"><a href ="contact.php"> Contact Us</a></button>
 		</ul>
 </div>
 	</nav>
@@ -76,8 +76,8 @@
 </div>
 <div class = "footer">
 	<div><li><a href = "https://github.com/AmaanSingh"><img src="glyphicons-social-22-github.png"></li></div>
-	<div><li><a href = "contact.html"><img src="glyphicons-social-14-e-mail-envelope.png"></a></li></div>
-	<div><li><a href = "disclaimer.html">Disclaimer</a></li></div>
+	<div><li><a href = "contact.php"><img src="glyphicons-social-14-e-mail-envelope.png"></a></li></div>
+	<div><li><a href = "disclaimer.php">Disclaimer</a></li></div>
 	<div class = "computer"><img src="logo.png"></div>
 	<div class = "jshs"><img src = "jshslogo.png"></div>
 
