fb7391caac4d76c975dd8822d183d03dab3d8f20
AmaanSingh <42991924+AmaanSingh@users.noreply.github.com>
2018-11-21 00:39:58 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -45,38 +45,14 @@
 <div class = "Sign">
 	<h1> PGC</h1>
 </div>
-<div id="contact" action="<?_SERVER['PHP_SELF']; ?>" method="post">
-  <div class="container">
-    <div class="col-md-8">
-      <div class="row">
-        <div class="section-title">
-          <h2>Get In Touch</h2>
-          <p>Please fill out the form below to send us an email and we will get back to you as soon as possible.</p>
-        </div>
-        <form name="sentMessage" id="contactForm" novalidate>
-          <div class="row">
-            <div class="col-md-6">
-              <div class="form-group">
-                <input type="text" id="name" class="form-control" placeholder="Name" required="required">
-                <p class="help-block text-danger"></p>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div class="form-group">
-                <input type="email" id="email" class="form-control" placeholder="Email" required="required">
-                <p class="help-block text-danger"></p>
-              </div>
-            </div>
-          </div>
-          <div class="form-group">
-            <textarea name="message" id="message" class="form-control" rows="4" placeholder="Message" required></textarea>
-            <p class="help-block text-danger"></p>
-          </div>
-          <div id="success"></div>
-          <button type="submit" class="btn btn-custom btn-lg">Send Message</button>
-        </form>
-      </div>
-    </div>
+<form class="contact-form" action="contact_me.php" method="post">
+	<input type="text" name= "name" placeholder="Full name">
+	<input type="text" name= "mail" placeholder="Your e-mail">
+	<input type="text" name= "subject" placeholder="Subject">
+	<textarea type="message" placeholder="Message"></textarea>
+	<button type="submit" name= "Sumbit">Send Mail</button>
+</form>
+
 <div class = "footer">
 	<div><li><a href = "https://github.com/AmaanSingh"><img src="glyphicons-social-22-github.png"></li></div>
 	<div><li><a href = "contact.php"><img src="glyphicons-social-14-e-mail-envelope.png"></a></li></div>
