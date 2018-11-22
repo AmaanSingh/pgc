a7550b6babf7e848a731dac0ec8acd3fb29ad50a
AmaanSingh <42991924+AmaanSingh@users.noreply.github.com>
2018-11-21 16:00:43 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,7 +5,6 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="stylesheet" type="text/css" href="index.css">
-	<?php include('contact_me.php'); ?>
 	<link rel="stylesheet" href="https://use.typekit.net/njb6zth.css">
 	<link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
 	<meta name="viewpoint" content="width=device-width, initial-scale=1.0">
