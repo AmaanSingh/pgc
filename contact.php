356491dd0432613705691da344e945caa0a484e9
AmaanSingh <42991924+AmaanSingh@users.noreply.github.com>
2018-11-25 19:15:53 -0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -15,6 +15,8 @@
     enable_page_level_ads: true
   });
 </script>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 </head>
 <body>
 <div id="main">
@@ -107,5 +109,12 @@
 	<div class = "computer"><img src="logo.png"></div>
 	<div class = "jshs"><img src = "jshslogo.png"></div>
 </div>
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/SmoothScroll.js"></script>
+<script type="text/javascript" src="js/nivo-lightbox.js"></script>
+<script type="text/javascript" src="js/jqBootstrapValidation.js"></script>
+<script type="text/javascript" src="js/contact_me.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
